1a4415c549b1ff577c00035952fcbf41c1937c1a
pavmeth22 <104300925+Nik2014@users.noreply.github.com>
2024-04-25 20:15:58 +0300

    Bulgaria only

--- download.py
+++ download.py
@@ -48,7 +48,7 @@ shutil.rmtree('shapefile')
 
 
 #Get the data only for Greece
-el_gdf = gdf[gdf['country'] == 'EL']
+el_gdf = gdf[gdf['country'] == 'BG']
 
 # Define the destination path for saving the GeoDataFrame
 destination_path = os.path.join('el_gdf.geojson')
